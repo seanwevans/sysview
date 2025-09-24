882558a02c15cb6e0416d9fa05e1e8bd724da5d6
Sean Evans <sean.w.evans@gmail.com>
2025-09-24 13:20:05 -0400

    Track per-entity syscall counts

--- tests/test_bpf_generation.py
+++ tests/test_bpf_generation.py
@@ -11,6 +11,7 @@ import sysview
 def _create_monitor(cfg):
     monitor = sysview.SyscallMonitor.__new__(sysview.SyscallMonitor)
     monitor.config = cfg
+    monitor.group_by = "pid"
     return monitor
 
 
@@ -26,7 +27,20 @@ def test_generate_bpf_program_only_enabled_syscalls():
     # Should contain BPF map and function for 'read'
     assert 'BPF_HASH(read_count' in program
     assert 'trace_read_entry' in program
+    assert 'bpf_get_current_pid_tgid' in program
+    assert 'key = id >> 32;' in program
 
     # Disabled syscalls should not appear
     assert 'BPF_HASH(write_count' not in program
     assert 'trace_write_entry' not in program
+
+
+def test_generate_bpf_program_tid_grouping():
+    cfg = sysview.SyscallConfig()
+    monitor = _create_monitor(cfg)
+    monitor.group_by = "tid"
+
+    program = sysview.SyscallMonitor.generate_bpf_program(monitor)
+
+    assert 'key = id;' in program
+    assert 'key = id >> 32;' not in program
